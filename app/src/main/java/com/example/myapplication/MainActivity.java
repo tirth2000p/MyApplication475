f7c8de89034f9f87be75f2c1b03dd708724cf1c7
unknown <tirth2000p@gmail.com>
2022-03-02 13:07:28 -0500

    bug fix

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* ************************************************************************** */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
