3eb335d68be9cffa008aada7579997c80cf1d1b9
chuanzheng55 <100614122+chuanzheng55@users.noreply.github.com>
2022-04-06 18:00:13 -0400

    theme

--- app/src/main/java/com/example/myapplication/settingsPage.java
+++ app/src/main/java/com/example/myapplication/settingsPage.java
@@ -2,14 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class settingsPage extends AppCompatActivity {
+
+    private boolean nightMode = false;
 
     private boolean Sound = true;
     String SoundVal = "Sound: On";
@@ -24,8 +30,38 @@ public class settingsPage extends AppCompatActivity {
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner); // gotta work on spinner settings
 
-        Button buttonTheme = findViewById(R.id.theme);
 
+        ToggleButton switchTheme = findViewById(R.id.theme);
+        SharedPreferences sharedPreferences = getSharedPreferences("save", MODE_PRIVATE);
+        switchTheme.setChecked(sharedPreferences.getBoolean("value", true));
+        if (switchTheme.isChecked()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        switchTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                boolean clicked = switchTheme.isChecked();
+                if(clicked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    SharedPreferences.Editor editor = getSharedPreferences("save", MODE_PRIVATE).edit();
+                    editor.putBoolean("value", true);
+                    editor.apply();
+                    switchTheme.setChecked(true);
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    SharedPreferences.Editor editor = getSharedPreferences("save", MODE_PRIVATE).edit();
+                    editor.putBoolean("value",false);
+                    editor.apply();
+                    switchTheme.setChecked(false);
+                }
+            }
+        });
 
 
         Button button3 = findViewById(R.id.button3);
